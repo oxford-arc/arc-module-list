3715ecc83b67e28770b973680bc73cc14429af65
oxford-arc <66992362+oxford-arc@users.noreply.github.com>
2024-05-03 09:53:42 +0100

    Update conf.py
    change favicons

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "sphinx_favicon",
 ]
 
 intersphinx_mapping = {
@@ -29,9 +30,16 @@ templates_path = ['_templates']
 pygments_style = 'sphinx'
 
 # -- Options for HTML output
+favicons = [
+    "favicon16.png",
+    "favicon32.png",
+    "favicon96.png",
+    "favicon160.png",
+    "arc_icon.svg",
+]
 
 html_theme = 'sphinx_rtd_theme'
-html_favicon = 'favicon.ico'
+#html_favicon = 'favicon.ico'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
